7995066ea804ca48f7a555ea8ca0ff25d89da7e1
Masud <masudhasan@google.com>
2020-12-23 15:13:58 -0500

    itn short graph traversal - re do

--- src/main/java/MisrememberedItn.java
+++ src/main/java/MisrememberedItn.java
@@ -1,17 +1,13 @@
 import com.google.common.collect.ImmutableList;
-import com.google.common.collect.ImmutableMap;
 import com.google.common.collect.ImmutableMultimap;
 import java.util.Collection;
-import java.util.Collections;
 import java.util.Comparator;
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.PriorityQueue;
 import java.util.Set;
 import java.util.stream.IntStream;
-import java.util.stream.Stream;
 
 class Cities{
     String city;
@@ -35,70 +31,79 @@ class HeapEntry{
 
 public class MisrememberedItn {
     Set<String> cities;
-    ImmutableMultimap<String, String> cityGraph;
-    Map<Cities, Cities> backconnection;
-    PriorityQueue<HeapEntry>  minHeap = new PriorityQueue<>(Comparator.comparingInt(d->d.distance));
-    public static int calculateDistance(String c1, String c2){
-        if(c1.length()!=c2.length()){ return -1; }
-        return IntStream.range(0,c1.length()).map(i->c1.charAt(i)==c2.charAt(i)?0:1).sum();
+    ImmutableMultimap<String,String> connectionGraph;
+    HashMap<City, City> backtrack;
+    public  int nameDistance(String s1, String s2){
+        return IntStream.range(0, s1.length()).map(i->s1.charAt(i)==s2.charAt(i)?0:1).sum();
+    }
+
+    class City {
+        String name;
+        int index;
+        public City(String name, int index){
+            this.name = name;
+            this.index = index;
+        }
+    }
+
+    class HeapEntry{
+        int distance;
+        City city;
+        public HeapEntry(int distance, City city){
+            this.distance = distance;
+            this.city = city;
+
+        }
+
     }
 
     public MisrememberedItn(Collection<HashMap<String, String>> cityConnections){
-        ImmutableMultimap.Builder<String, String> connectionnBuilder = ImmutableMultimap.builder();
-        cityConnections.forEach(city->{
-            city.entrySet().forEach(c->{
+        ImmutableMultimap.Builder connections = ImmutableMultimap.builder();
+        cityConnections.forEach(cc->{
+            cc.entrySet().forEach(c->{
                 cities.add(c.getKey());
                 cities.add(c.getValue());
-                connectionnBuilder.put(c.getKey(), c.getValue());
-                connectionnBuilder.put(c.getValue(), c.getKey());
+                connections.put(c.getKey(),c.getValue());
+                connections.put(c.getKey(),c.getValue());
+
+
             });
         });
-
-        cityGraph =connectionnBuilder.build();
-        backconnection = new HashMap<>();
-
+        connectionGraph = connections.build();
+        backtrack = new HashMap<>();
     }
 
     public List<String> findClosestRoute(List<String> itn){
-        ImmutableList.Builder listBuilder = ImmutableList.builder();
-        if(itn.isEmpty()){
-            return listBuilder.build();
-        }
+      ImmutableList.Builder routeList = ImmutableList.builder();
+      PriorityQueue<HeapEntry> minHeap = new PriorityQueue<>(Comparator.comparingInt(he->he.distance));
+      cities.forEach(city->{
+          minHeap.offer(new HeapEntry(nameDistance(city,itn.get(0)),new City(city,0))));
+      });
+      while (!minHeap.isEmpty()){
+          HeapEntry currntState  = minHeap.poll();
+          int index = currntState.city.index;
+          if(index==itn.size()-1){
+              while (index >= 0){
+                  routeList.add(backtrack.get(currntState.city.name));
+                  index --;
+              }
+              return routeList.build();
+          }
 
-        cities.forEach(city->{
-            minHeap.offer(new HeapEntry(calculateDistance(city,itn.get(0)),new Cities(city,0)));
-        });
-        while (!minHeap.isEmpty()){
+          for (String nb : connectionGraph.get(currntState.city.name)){
 
-            HeapEntry current = minHeap.poll();
-            if(current.node.order==itn.size()-1){
+              if(backtrack.containsKey(new City(nb,index+1))) continue;
 
-                int index = current.node.order;
-                Cities currentCity  = current.node;
-                while (index>=0){
-                    listBuilder.add(backconnection.get(currentCity));
-                    index --;
-                }
-                return listBuilder.build();
+              backtrack.put(new City(nb, index+1),currntState.city);
 
-            }
+              minHeap.offer(new HeapEntry(currntState.distance+nameDistance(nb,itn.get(index+1)),
+                  new City(nb,index+1)));
 
-            for (String nb : cityGraph.get(current.node.city)){
-
-                if(backconnection.containsKey(new Cities(nb,current.node.order+1))){
-                    continue;
-                }
-                backconnection.put(new Cities(nb,current.node.order+1),current.node);
-
-                minHeap.offer(new HeapEntry(current.distance+calculateDistance(nb,
-                    itn.get(current.node.order+1)),new Cities(nb,current.node.order+1)));
+          }
+      }
 
 
-            }
-        }
-
-
-        return listBuilder.build();
+      return routeList.build();
     }
 
 
