b4bba5464c380e5e89ab0fdcc7e339558cfe9267
Masud <masudhasan@google.com>
2021-01-11 10:16:19 -0500

    Alertig system q2- n^2

--- src/main/java/AlertingSystem.java
+++ src/main/java/AlertingSystem.java
@@ -22,11 +22,26 @@ public class AlertingSystem {
     }
     return count==interval?true:false;
   }
+  public boolean hasAlertQ2(double[] rate){
+    int n = rate.length;
+    int maxArea =0;
+    for (int i =0; i<n; i++ ){
+      int minHeight = Integer.MAX_VALUE;
+      for (int j =i; j<n; j++){
+        minHeight = Math.min(minHeight, (int)(rate[j]));
+        int width = j -i +1;
+        maxArea = Math.max(maxArea,minHeight * width );
+
+      }
+    }
+    return maxArea>=threshold;
+  }
 
   public static void main (String args[]){
       AlertingSystem as = new AlertingSystem(250.0);
      double [] rate = new double[]
         {5, 10, 7, 15, 0, 60, 55, 60, 60, 60, 0, 5, 5};
-      System.out.println(as.hasAlertQ1(6,6, 55, rate));
+      //System.out.println(as.hasAlertQ1(6,6, 55, rate));
+     System.out.println(as.hasAlertQ2(rate));
   }
 }
