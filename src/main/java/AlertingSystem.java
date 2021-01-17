0f9910296b12310db8ceec9a4ad33cd80b754d7e
Masud <masudhasan@google.com>
2021-01-17 11:32:58 -0500

    grep again

--- src/main/java/AlertingSystem.java
+++ src/main/java/AlertingSystem.java
@@ -1,3 +1,6 @@
+import java.util.ArrayDeque;
+import java.util.Deque;
+
 public class AlertingSystem {
  double threshold;
   public AlertingSystem(double threshold){
@@ -22,6 +25,28 @@ public class AlertingSystem {
     }
     return count==interval?true:false;
   }
+
+  public boolean hasAlertQ3(double[] rate){
+    Deque<Integer> queue = new ArrayDeque<>();
+    queue.add(-1);
+    int maxArea =0;
+    int n = rate.length;
+    for (int i=0; i<n;i++){
+      while (queue.peek()!=-1 && rate[queue.peek()]<= rate[i]){
+       int currentHeight = (int) rate[queue.pop()];
+       int currentWidth = i - queue.peek() -1;
+       maxArea =  Math.max(maxArea, currentHeight * currentWidth);
+      }
+      queue.push(i);
+    }
+
+    while (queue.peek()!=-1){
+      int currentHeight = (int) rate[queue.pop()];
+      int currentWidth =  n - queue.peek() -1;
+      maxArea =  Math.max(maxArea, currentHeight * currentWidth);
+    }
+    return maxArea>=threshold;
+  }
   public boolean hasAlertQ2(double[] rate){
     int n = rate.length;
     int maxArea =0;
@@ -38,10 +63,11 @@ public class AlertingSystem {
   }
 
   public static void main (String args[]){
-      AlertingSystem as = new AlertingSystem(250.0);
+      AlertingSystem as = new AlertingSystem(250);
      double [] rate = new double[]
         {5, 10, 7, 15, 0, 60, 55, 60, 60, 60, 0, 5, 5};
-      //System.out.println(as.hasAlertQ1(6,6, 55, rate));
+     System.out.println(as.hasAlertQ1(6,6, 55, rate));
      System.out.println(as.hasAlertQ2(rate));
+     System.out.println(as.hasAlertQ3(rate));
   }
 }
---
+++ src/main/java/grepcontext/GrepContextP.java
@@ -0,0 +1,36 @@
+package grepcontext;
+
+import static java.util.Arrays.asList;
+
+import java.util.List;
+import java.util.function.Predicate;
+
+public class GrepContextP {
+
+  static void printMatches(List<String> lines, Predicate<String> matcher, int contextRequested){
+    int previousEnd =0;
+    for (int i =0; i< lines.size(); i++){
+      String word  = lines.get(i);
+      if (matcher.test(word)){
+        int start = i-contextRequested < 0 ?0:i-contextRequested;
+        int end =  i+contextRequested > lines.size()?lines.size():i+contextRequested;
+        start = Math.max(previousEnd,start);
+        for (int j = start ; j<= end; j++){
+          System.out.println(lines.get(j));
+        }
+        previousEnd = end+1;
+
+
+      }
+    }
+
+  }
+
+
+  public static void main (String args[]){
+    printMatches(
+        asList("organize", "it", "and", "make", "it", "accessible", "and", "useful"),
+        "it"::equals,
+        /*contextRequested=*/ 2);
+  }
+}
