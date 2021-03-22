74fa7c441d0bbb8df65c4a87aaa39f428e8cc6c1
Masud <masudhasan@google.com>
2021-03-21 21:08:04 -0400

    watermark ii

--- src/main/java/HighWaterMarkPQ.java
+++ src/main/java/HighWaterMarkPQ.java
@@ -1,20 +1,28 @@
 import java.util.Comparator;
+import java.util.HashSet;
 import java.util.PriorityQueue;
+import java.util.Set;
 
 public class HighWaterMarkPQ {
   int start;
   int hwm;
   PriorityQueue<Integer> minHeap;
+  Set<Integer> valueSet;
 
 
   public HighWaterMarkPQ(int start){
     this.start = start;
     this.hwm = this.start;
     minHeap = new PriorityQueue<>(Comparator.comparingInt(a->a));
+    valueSet = new HashSet<>();
   }
 
   public void ack(int n){
-    minHeap.add(n);
+    if(!valueSet.contains(n)) {
+      minHeap.add(n);
+      valueSet.add(n);
+    }
+
   }
 
   public  int getHWM(){
@@ -33,14 +41,14 @@ public class HighWaterMarkPQ {
     System.out.println(hw.getHWM());
     hw.ack(3);
     System.out.println(hw.getHWM());
-    hw.ack(7);
-    System.out.println(hw.getHWM());
-    hw.ack(5);
-    System.out.println(hw.getHWM());
-    hw.ack(6);
+    hw.ack(2);
     System.out.println(hw.getHWM());
     hw.ack(4);
     System.out.println(hw.getHWM());
+    // hw.ack(5);
+    // System.out.println(hw.getHWM());
+    // hw.ack(4);
+    // System.out.println(hw.getHWM());
 
 
   }
